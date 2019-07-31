f49f272b92bab25850e181fc52edbfae8b7d5dd7
Paolo Fermin <paolofermin@vt.edu>
2019-07-31 11:29:30 -0400

    moved transform script to proper location

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,25 +1,44 @@
 from paraview.simple import *
 import os
+import time as clock
+import multiprocessing
 
-velocity = 5
 
-fields = ['delT', 'Uz']
+start_time = clock.time()
+
+velocity = 1
+
+fields = ['delT', 'Uz', 'nut']
 
 print(fields)
 
 time_range = 1
-while os.path.exists(os.path.join(os.getcwd(),'insitu', fields[0] + '_%d.vtm'%time_range)):
+while os.path.exists(os.path.join(os.getcwd(),'insitu', fields[2] + '_%d.vtm'%time_range)):
 	time_range += 1
 
-print(time_range)
-for time in range(1,time_range + 1):
-	for field in fields:
-		print('Reading field %s for timestep %d' % (field, time))
-		reader = XMLMultiBlockDataReader(FileName=os.path.join(os.getcwd(),'insitu', field + '_%d.vtm'%time))
-		transform = Transform(Input=reader)
-		transform.Transform = 'Transform'
-		transform.Transform.Translate = [velocity * time, 0, 0]
-		
-		writer = servermanager.writers.XMLMultiBlockDataWriter(Input=transform, FileName=os.path.join(os.getcwd(),'transforms',field,field + 'transform_%d.vtm'%time))
 
-		writer.UpdatePipeline()
+def transform_function(time, field):
+	print('Reading field %s for timestep %d' % (field, time))
+	reader = XMLMultiBlockDataReader(FileName=os.path.join(os.getcwd(),'insitu', field + '_%d.vtm'%time))
+	transform = Transform(Input=reader)
+	transform.Transform = 'Transform'
+	transform.Transform.Translate = [velocity * time, 0, 0]
+	
+	writer = servermanager.writers.XMLMultiBlockDataWriter(Input=transform, FileName=os.path.join(os.getcwd(),'transforms',field,field + 'transform_%d.vtm'%time))
+
+	writer.UpdatePipeline()
+
+print(time_range)	#note that time range is 1 above the real value
+
+processes = []
+for time in range(1,time_range):
+	for field in fields:
+		p = multiprocessing.Process(target=transform_function, args=(time, field))
+		processes.append(p)
+		p.start()
+	
+for process in processes: 
+	process.join()
+	
+
+print('Execution time: {} seconds'.format(clock.time() - start_time))
